eda39accb7b45926930f2ac73024452225c51b05
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-03-21 17:17:26 +0100

    MO: update modules

--- ps_livetranslation.php
+++ ps_livetranslation.php
@@ -36,6 +36,8 @@ class Ps_Livetranslation extends Module
     /** Locale for fake language in Crowdin */
     CONST LIVETRANSLATION_LOCALE = 'en-UD';
 
+    private $languageIsInstalled;
+
     public function __construct()
     {
         $this->name = 'ps_livetranslation';
@@ -47,11 +49,13 @@ class Ps_Livetranslation extends Module
         parent::__construct();
 
         $this->displayName = $this->trans('Live translation', array(), 'Modules.Livetranslation.Admin');
-        $this->description = $this->trans('Live translation module with Crowdin integration!', array(), 'Modules.Livetranslation.Admin');
+        $this->description = $this->trans('Contribute to the translation of PrestaShop directly from your own shop and administration panel.', array(), 'Modules.Livetranslation.Admin');
 
         $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
 
-        if (!Tools::isSubmit('submitLiveTranslation')) {
+        $this->languageIsInstalled = $this->checkIfLanguageIsInstalled();
+
+        if ($this->languageIsInstalled) {
             $this->checkLiveTranslation();
         }
     }
@@ -60,183 +64,168 @@ class Ps_Livetranslation extends Module
     {
         if ($success = Language::downloadAndInstallLanguagePack(self::LIVETRANSLATION_ISO, $version = _PS_VERSION_, $params = null, $install = true)) {
             Language::loadLanguages();
-
-            Configuration::updateValue('LIVETRANSLATION_BACKOFFICE', false);
-            Configuration::updateValue('LIVETRANSLATION_FRONTOFFICE', false);
-
-            return parent::install()
-                && $this->registerHook('displayHeader')
-                && $this->registerHook('displayBackOfficeHeader');
-
         } else {
-            $this->_errors[] = $this->trans('Unable to install the module because of translation Upside Down.', array(), 'Modules.Livetranslation.Admin');
+            $this->_errors['cannot_install'] = $this->trans('Unable to install the module: the English Upside Down language cannot be installed.', array(), 'Modules.Livetranslation.Admin');
             return false;
         }
+
+        return parent::install()
+            && $this->registerHook('displayHeader')
+            && $this->registerHook('displayBanner')
+            && $this->registerHook('displayAdminAfterHeader')
+            && $this->registerHook('displayBackOfficeHeader');
     }
 
     public function uninstall()
     {
-        Configuration::updateValue('LIVETRANSLATION_BACKOFFICE', false);
-        Configuration::updateValue('LIVETRANSLATION_FRONTOFFICE', false);
-
-        $this->disableLiveTranslation();
+        $this->disableLiveTranslation(true);
 
         return parent::uninstall();
     }
 
     public function getContent()
     {
-        $output = '';
+        $output = '<div class="panel">';
 
-        if (Tools::isSubmit('submitLiveTranslation')) {
+        $output .= '<div class="alert alert-info">' .
+            '<p>'.$this->trans('This module makes it possible to translate PrestaShop directly from its various pages (back and front office).', array(), 'Modules.Livetranslation.Admin').'</p>';
 
-            Configuration::updateValue('LIVETRANSLATION_BACKOFFICE', (bool) Tools::getValue('LIVETRANSLATION_BACKOFFICE'));
-            Configuration::updateValue('LIVETRANSLATION_FRONTOFFICE', (bool) Tools::getValue('LIVETRANSLATION_FRONTOFFICE'));
+        $output .= '<p>'.$this->trans('It sends these in-context translations immediately to the [1]PrestaShop translation project[/1] on Crowdin: this is to contribute to our community translations, not to translate or customize your own shop.', array(
+                '[1]' => '<a href="https://crowdin.com/project/prestashop-official" target="_blank">',
+                '[/1]' => '</a>',
+            ), 'Modules.Livetranslation.Admin').'</p>';
 
-            $output = $this->displayConfirmation($this->trans('The settings have been updated.', array(), 'Admin.Notifications.Success'));
+        $output .= '<p>'.$this->trans('If you don’t have a Crowdin account yet, [1]join now[/1]!', array(
+                '[1]' => '<a href="https://crowdin.com/project/prestashop-official" target="_blank">',
+                '[/1]' => '</a>',
+            ), 'Modules.Livetranslation.Admin').'</p>';
+
+        $output .= '<p>'.$this->trans('To be able to work, the module needs the virtual language “English Upside Down”. It is installed in your shop, but is not active, and not available to your customers.', array(), 'Modules.Livetranslation.Admin').'</p>';
+
+        $output .= '</div>';
+
+        if ($this->languageIsInstalled) {
+            $output .= '<h4>'.$this->trans('Translate PrestaShop using the live translation module:', array(), 'Admin.Livetranslation.Admin').'</h4>';
+
+            $output .= '<form class="form-horizontal"><div class="form-group">
+                <label class="control-label col-lg-1">'.$this->trans('Back office', array(), 'Admin.Livetranslation.Admin').'</label>
+                <div class="col-lg-4">
+                    <a class="btn btn-primary btn-sm" 
+                        href="'.$this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name, 'live_translation' => 1)).'" 
+                        title="'.$this->trans('Translate', array(), 'Admin.Livetranslation.Admin').'">' .
+                        $this->trans('Translate', array(), 'Admin.Livetranslation.Admin') .
+                    '</a>
+                </div>
+            </div>';
+
+            $liveTranslationLanguage = new Language((int)Language::getIdByIso(self::LIVETRANSLATION_ISO));
+            $output .= '<div class="form-group">
+                <label class="control-label col-lg-1">'.$this->trans('Front office', array(), 'Admin.Livetranslation.Admin').'</label>
+                <div class="col-lg-4">
+                    <a class="btn btn-primary btn-sm" 
+                        href="'.$this->context->link->getBaseLink($this->context->shop->id).$liveTranslationLanguage->iso_code.'/?live_translation=1" 
+                        title="'.$this->trans('Translate', array(), 'Admin.Livetranslation.Admin').'"
+                        target="_blank">' .
+                    $this->trans('Translate', array(), 'Admin.Livetranslation.Admin') .
+                    '</a>
+                </div>
+            </div></form>';
         }
 
-        return $output.$this->renderForm();
+        $output .= $this->displayErrors();
+
+        $output .= '</div>';
+
+        return $output;
     }
 
-    public function renderForm()
+    private function displayErrors()
     {
-        $defaultLanguage = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
-        $liveTranslationLanguage = new Language((int)Language::getIdByIso(self::LIVETRANSLATION_ISO));
+        $output = '';
 
-        $fields_form = array(
-            'form' => array(
-                'legend' => array(
-                    'title' => $this->trans('Settings', array(), 'Admin.Global'),
-                    'icon' => 'icon-cogs',
-                ),
-                'description' => $this->trans('The Live Translation module makes it possible to translate PrestaShop right from its various pages (back and front office). It send these in-context translations directly to the PrestaShop translation project on Crowdin: this is for contributing to our community translations, not to translate or customize your own shop.', array(), 'Admin.Livetranslation.Admin'),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->trans('Enable live translation on your back office', array(), 'Modules.Livetranslation.Admin'),
-                        'name' => 'LIVETRANSLATION_BACKOFFICE',
-                        'class' => 'fixed-width-xs',
-                        'desc' =>
-                            $this->trans('After enabled this option, you can edit your back office by clicking [1]here[/1]', array(
-                                '[1]' => '<a href="'.$this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name, 'live_translation' => 1)).'">',
-                                '[/1]' => '</a>',
-                                ),
-                                'Modules.Livetranslation.Admin'
-                            ).' '.
-                            $this->trans('To disable live translation, [1]click here[/1].', array(
-                                '[1]' => '<a href="'.$this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name, 'disable_live_translation' => 1)).'">',
-                                '[/1]' => '</a>',
-                            ),
-                                'Modules.Livetranslation.Admin'
-                            ),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => 1,
-                                'label' => $this->trans('Yes', array(), 'Admin.Global'),
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => 0,
-                                'label' => $this->trans('No', array(), 'Admin.Global'),
-                            ),
-                        ),
-                    ),
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->trans('Enable live translation on your front office', array(), 'Modules.Livetranslation.Admin'),
-                        'name' => 'LIVETRANSLATION_FRONTOFFICE',
-                        'class' => 'fixed-width-xs',
-                        'desc' => $this->trans('Enable if you wish live translation to be active on your front office.', array(), 'Modules.Livetranslation.Admin').
-                            '<br>'.
-                            $this->trans('After enabled this option, you can edit your front office by clicking [1]here[/1].', array(
-                                '[1]' => '<a href="'.$this->context->link->getBaseLink($this->context->shop->id).$liveTranslationLanguage->iso_code.'/?live_translation=1" target="_blank">',
-                                '[/1]' => '</a>',
-                                ),
-                                'Modules.Livetranslation.Admin'
-                            ).' '.
-                            $this->trans('To disable live translation, [1]click here[/1].', array(
-                                '[1]' => '<a href="'.$this->context->link->getBaseLink($this->context->shop->id).$defaultLanguage->iso_code.'/?disable_live_translation=1" target="_blank">',
-                                '[/1]' => '</a>',
-                            ),
-                                'Modules.Livetranslation.Admin'
-                            ),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => 1,
-                                'label' => $this->trans('Yes', array(), 'Admin.Global'),
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => 0,
-                                'label' => $this->trans('No', array(), 'Admin.Global'),
-                            ),
-                        ),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->trans('Save', array(), 'Admin.Actions'),
-                ),
-            ),
-        );
+        if (!empty($this->_errors)) {
+            $output .= '<div class="alert alert-danger">';
+            foreach ($this->_errors as $error) {
+                $output .= '<p>'.$error.'</p>';
+            }
+            $output .= '</div>';
+        }
 
-        $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
-
-        $helper = new HelperForm();
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->default_form_language = $lang->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitLiveTranslation';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFieldsValues(),
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
-
-        return $helper->generateForm(array($fields_form));
+        return $output;
     }
 
-    /**
-     * Get config values for live translation
-     *
-     * @return array
-     */
-    public function getConfigFieldsValues()
-    {
-        return array(
-            'LIVETRANSLATION_BACKOFFICE' => Tools::getValue('LIVETRANSLATION_BACKOFFICE', (bool) Configuration::get('LIVETRANSLATION_BACKOFFICE')),
-            'LIVETRANSLATION_FRONTOFFICE' => Tools::getValue('LIVETRANSLATION_FRONTOFFICE', (bool) Configuration::get('LIVETRANSLATION_FRONTOFFICE')),
-        );
-    }
-
-    /**
-     * Hook for header shop (front)
-     *
-     * @param $params
-     */
     public function hookDisplayHeader($params)
     {
-        if ((bool) Configuration::get('LIVETRANSLATION_FRONTOFFICE') && $this->isLiveTranslationActive()) {
+        if ($this->isLiveTranslationActive()) {
             $this->context->controller->registerJavascript('modules-livetranslation', 'modules/'.$this->name.'/js/livetranslation.js', ['position' => 'bottom', 'priority' => 110]);
         }
     }
 
-    /**
-     * Hook for Back office header
-     *
-     * @param $params
-     */
+    public function hookDisplayBanner($params)
+    {
+        if ($this->isLiveTranslationActive()) {
+            $defaultLanguage = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+
+            return $this->displayBannerTpl(
+                array('link' => $this->context->link->getBaseLink($this->context->shop->id).$defaultLanguage->iso_code.'/?disable_live_translation=1')
+            );
+        }
+
+        return false;
+    }
+
     public function hookDisplayBackOfficeHeader($params)
     {
-        if ((bool) Configuration::get('LIVETRANSLATION_BACKOFFICE') && $this->isLiveTranslationActive()) {
+        if ($this->isLiveTranslationActive()) {
             $this->context->controller->addJS($this->_path.'js/livetranslation.js', 'all');
         }
+    }
+
+    public function hookDisplayAdminAfterHeader($params)
+    {
+        if ($this->isLiveTranslationActive()) {
+            return $this->displayBannerTpl(
+                array('link' => $this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name, 'disable_live_translation' => 1)))
+            );
+        }
+
+        return false;
+    }
+
+    private function displayBannerTpl($data)
+    {
+        $this->smarty->assign($data);
+
+        return $this->display(__FILE__, 'views/templates/hook/ps_livetranslation.tpl');
+    }
+
+    /**
+     * Check if live translation mode is enabled (checking cookie)
+     *
+     * @return bool
+     */
+    private function isLiveTranslationActive()
+    {
+        return !empty($this->context->cookie->isLiveTranslationActive);
+    }
+
+    /**
+     * Check if upside down language is installed, if not, try to install it, then make an error
+     *
+     * @return bool
+     */
+    private function checkIfLanguageIsInstalled()
+    {
+        if (!(int)Language::getIdByIso(self::LIVETRANSLATION_ISO)) {
+            if ($success = Language::downloadAndInstallLanguagePack(self::LIVETRANSLATION_ISO, $version = _PS_VERSION_, $params = null, $install = true)) {
+                Language::loadLanguages();
+            } else {
+                $this->_errors['cannot_install'] = $this->trans('Live translation cannot be enabled. The English Upside Down language is missing and cannot be installed. Please reset the module to try again. ', array(), 'Modules.Livetranslation.Admin');
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -261,10 +250,12 @@ class Ps_Livetranslation extends Module
 
     /**
      * Disable live translation mode (using cookie)
+     *
+     * @param $onInstall bool
      */
-    private function disableLiveTranslation()
+    private function disableLiveTranslation($onInstall = false)
     {
-        $this->handleLiveTranslationMode(false, (int)Configuration::get('PS_LANG_DEFAULT'));
+        $this->handleLiveTranslationMode(false, (int)Configuration::get('PS_LANG_DEFAULT'), $onInstall);
     }
 
     /**
@@ -272,8 +263,9 @@ class Ps_Livetranslation extends Module
      *
      * @param $state bool
      * @param $idLang int
+     * @param $onInstall bool
      */
-    private function handleLiveTranslationMode($state, $idLang)
+    private function handleLiveTranslationMode($state, $idLang, $onInstall = false)
     {
         $lang = new Language((int)$idLang);
 
@@ -289,30 +281,17 @@ class Ps_Livetranslation extends Module
             }
         }
 
-        $this->redirectLiveTranslation($lang->iso_code);
-    }
-
-    /**
-     * Check if live translation mode is enabled (checking cookie)
-     *
-     * @return bool
-     */
-    private function isLiveTranslationActive()
-    {
-        return !empty($this->context->cookie->isLiveTranslationActive);
+        // only redirect when is not on install
+        if (true !== $onInstall && $state === false) {
+            $this->redirectLiveTranslation();
+        }
     }
 
     /**
      * Redirect after changing live translation mode
-     *
-     * @param $isoCode string
      */
-    private function redirectLiveTranslation($isoCode)
+    private function redirectLiveTranslation()
     {
-        if (isset($this->context->employee) && !empty($this->context->employee->id)) {
-            Tools::redirect($this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name)));
-        } else {
-            Tools::redirect($this->context->link->getBaseLink($this->context->shop->id).$isoCode.'/');
-        }
+        Tools::redirect($this->context->link->getAdminLink('AdminModules', true, null, array('configure' => $this->name)));
     }
 }
